f37d91036cb1dff80cc3588fb5e03076f35c6129
Mehmetkozanoglu <108228905+Mehmetkozanoglu@users.noreply.github.com>
2023-02-17 18:06:16 +0300

    Added Delete Method

--- BookStore/WebApi/Controllers/BookController.cs
+++ BookStore/WebApi/Controllers/BookController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using WebApi.BookOperations.CreateBook;
+using WebApi.BookOperations.DeleteBook;
 using WebApi.BookOperations.GetBookDetail;
 using WebApi.BookOperations.GetBooks;
 using WebApi.BookOperations.UpdateBook;
@@ -84,12 +85,17 @@ namespace WebApi.Controllers
        [HttpDelete("{id}")]
        public IActionResult DeleteBook(int id)
        {
-        var book = _context.Books.SingleOrDefault(x => x.Id == id);
-        if(book is null)
-        return BadRequest();
-
-        _context.Books.Remove(book);
-        _context.SaveChanges();
+        try
+        {
+          DeleteBookCommand command = new DeleteBookCommand(_context);
+          command.BookId = id;
+          command.Handle();
+        }
+        catch (Exception ex)
+        {
+          
+          return BadRequest(ex.Message);
+        }
         return Ok();
        }
     }
